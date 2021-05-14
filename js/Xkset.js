a4d68d2e656574b568f1ecbfb8022eccc1967886
holzora <40710760+holzora@users.noreply.github.com>
2021-05-15 00:34:14 +0800

    Update Xkset.js

--- js/Xkset.js
+++ js/Xkset.js
@@ -1,3 +1,4 @@
+/*
 function disptime(){  
     var today=new Date();  
     var hh=today.getHours();  
@@ -12,7 +13,8 @@ function show_runtime(){window.setTimeout("show_runtime()",1000);
     Y=new Date();T=(Y.getTime()-X.getTime());M=24*60*60*1000;
     a=T/M;A=Math.floor(a);b=(a-A)*24;B=Math.floor(b);c=(b-B)*60;C=Math.floor((b-B)*60);D=Math.floor((c-C)*60);
     runtime_span.innerHTML="本站已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();
- 
+*/
+
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     mini: false,
