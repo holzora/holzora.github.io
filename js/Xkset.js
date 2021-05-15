43aaa877ca756e38044fa4b2b1dd8726ae959c52
holzora <40710760+holzora@users.noreply.github.com>
2021-05-15 12:36:58 +0800

    Update Xkset.js

--- js/Xkset.js
+++ js/Xkset.js
@@ -15,30 +15,6 @@ function show_runtime(){window.setTimeout("show_runtime()",1000);
     runtime_span.innerHTML="本站已运行: "+A+"天"+B+"小时"+C+"分"+D+"秒"}show_runtime();
 */
 
-const colorThief = new ColorThief();
-const image = new Image();
-const xhr = new XMLHttpRequest();
-const setTheme = (index) => {
-    if (!ap.list.audios[index].theme) {
-        xhr.onload = function(){
-            let coverUrl = URL.createObjectURL(this.response);
-            image.onload = function(){
-                let color = colorThief.getColor(image);
-                ap.theme(`rgb(${color[0]}, ${color[1]}, ${color[2]})`, index);
-                URL.revokeObjectURL(coverUrl)
-            };
-            image.src = coverUrl;
-        }
-        xhr.open('GET', ap.list.audios[index].cover, true);
-        xhr.responseType = 'blob';
-        xhr.send();
-    }
-};
-setTheme(ap.list.index);
-ap.on('listswitch', (index) => {
-    setTheme(index);
-});
-
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     mini: false,
